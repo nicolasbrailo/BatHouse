b5023df8b7c3b997514dca0187ac491c645a7939
Nico <nicolasbrailo@gmail.com>
2019-04-18 11:25:33 +0200

    Refactor & pretty icons for shortcuts

---
+++ webapp/app.js
@@ -0,0 +1,56 @@
+
+class DumbHouseApp {
+    constructor() {
+        this.things_app = new ThingsApp("/ZMF/");
+        this.things_templates_ready = ThingsApp.init_all_templates("/ZMF/webapp/");
+        this.is_ready = $.Deferred();
+        this.scenes_tmpl_ready = $.Deferred();
+
+        var self = this;
+        $(document).ready(function(){
+            $.when(self.templates_ready).then(function() {
+                $.when(self.things_app.is_ready).then(function() {
+                    $.when(self.scenes_tmpl_ready.is_ready).then(function() {
+                        self.is_ready.resolve(); 
+                    });
+                });
+            });
+        });
+
+        var self = this;
+        $.ajax({
+            url: "scenes_view.html",
+            cache: false,
+            type: 'get',
+            dataType: 'html',
+            success: function(tmpl) {
+                if (!Handlebars || !Handlebars.registerHelper) {
+                    console.error("Handlebars plugin not found");
+                }
+
+                self.render_scenes = Handlebars.compile(tmpl);
+                self.scenes_tmpl_ready.resolve();
+            }
+        });
+    }
+
+    show_list_of(thing_type, element_selector) {
+        for (var thing of this.things_app.get_things_of_type(thing_type)) {
+            $(element_selector).append(thing.create_ui());
+        }
+    }
+
+    show_scenes(template_element_selector) {
+        var self = this;
+        $.ajax({
+            url: '/scenes/list',
+            cache: false,
+            type: 'get',
+            dataType: 'json',
+            success: function(scenes) {
+                $('#scene_list').replaceWith(self.render_scenes(scenes));
+            }
+        });
+    }
+}
+
