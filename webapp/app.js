e7c33e893782eaa1175a11944ce8a4a62b7d088d
Nico <nicolasbrailo@gmail.com>
2019-04-27 21:31:16 +0200

    UI Nico-wide

--- webapp/app.js
+++ webapp/app.js
@@ -1,4 +1,15 @@
 
+// http://dygraphs.com/options.html
+function dygraph_helper_horizontalLine(ctx, graph, y_val, color) {
+  var xl = graph.toDomCoords(0,y_val);
+  ctx.strokeStyle= color;
+  ctx.beginPath();
+  ctx.moveTo(xl[0],xl[1]);
+  ctx.lineTo(9e22,xl[1]);
+  ctx.closePath();
+  ctx.stroke();      
+}
+
 class DumbHouseApp {
     constructor() {
         this.things_app = new ThingsApp("/ZMF/", "/ZMF/webapp/", [Lamp, MediaPlayer, MqttDeviceInfo]);
@@ -135,6 +146,113 @@ class DumbHouseApp {
         });
     }
 
+    toggle_Local_Sensors_Plot(dygraph_element_id) {
+        $('#' + dygraph_element_id ).toggle();
+        if ($('#' + dygraph_element_id ).is(':visible')) {
+            this.plot_local_sensors(dygraph_element_id);
+        }
+    }
+
+    plot_local_sensors(dygraph_element_id) {
+        var opts = {
+            labels: [ 'Date', 'Temperature', 'CO2'],
+            ylabel: 'CO2 PPM',
+            y2label: 'Temperature (C)',
+            highlightCircleSize: 2,
+            strokeWidth: 1,
+            highlightSeriesOpts: {
+                strokeWidth: 3,
+                strokeBorderWidth: 1,
+                highlightCircleSize: 5
+            },
+            series: {
+                'Temperature': {'axis': 'y2'/*, 'plotter': smoothPlotter*/},
+                'CO2': {'axis': 'y', 'plotter': smoothPlotter}
+            },
+            axes: { x: {
+                valueFormatter: function(d) { return new Date(d*1000); },
+                axisLabelFormatter: function(d, gran, opts) {
+                    var t = new Date(d*1000);
+                    return t.getHours() + ":" + t.getMinutes() + " " + t.getDay() + "/" + t.getMonth();
+                }
+            }},
+        };
+
+        new Dygraph(
+            document.getElementById(dygraph_element_id),
+            "co2_history.csv?random=" + new Date().getTime(),
+            opts);
+    }
+
+    /*******************************************************/
+    /* WEATHER INTEGRATION */
+    /*******************************************************/
+    toggle_Weather_Plots(rain_forecast_element, temp_forecast_element, hours){
+        $('#' + rain_forecast_element).toggle();
+        $('#' + temp_forecast_element).toggle();
+        if ($('#' + temp_forecast_element).is(':visible')) {
+            this.plot_temp_forecast(temp_forecast_element, hours);
+            this.plot_rain(rain_forecast_element);
+        }
+    }
+
+    plot_temp_forecast(dygraph_element_id, hours) {
+        $.ajax({
+            url: "https://api.meteoplaza.com/v2/meteo/completelocation/5237.494?lang=en",
+            cache: false,
+            type: 'get',
+            dataType: 'json',
+            success: function(forecast) {
+                    var csv = "Date,Temp (C)\n";
+                    for(var h of forecast.Hourly.slice(0, hours)) {
+                        csv += h.ValidDt + "," + h.TTTT + "\n";
+                    }
+
+                    new Dygraph(document.getElementById(dygraph_element_id), csv,
+                        {fillGraph: true});
+                }
+        });
+    }
+
+    plot_rain(dygraph_element_id) {
+        $.ajax({
+            url: "https://cdn-secure.buienalarm.nl/api/3.4/forecast.php?lat=52.37064&lon=4.94223&region=nl&unit=mm/u",
+            cache: false,
+            type: 'get',
+            dataType: 'json',
+            success: function(forecast) {
+                var csv = "Date,Rain(mm/h)\n";
+                var t = forecast.start;
+                for (var rain of forecast.precip) {
+                    csv += t + "," + rain + "\n";
+                    t += forecast.delta;
+                }
+
+                var dygraph_opts ={
+                    series: {'Rain(mm/h)': {'plotter': smoothPlotter}},
+                    axes: {
+                        y: {valueRange: [0, 5],},
+                        x: {
+                            valueFormatter: function(d) { return new Date(d*1000); },
+                            axisLabelFormatter: function(d, gran, opts) {
+                                var t = new Date(d*1000);
+                                return t.getHours() + ":" + t.getMinutes();
+                            }
+                        },
+                    },
+                    underlayCallback: function(ctx, area, graph) {
+                        dygraph_helper_horizontalLine(ctx, graph, 0.25, 'green');
+                        dygraph_helper_horizontalLine(ctx, graph, 1, 'yellow');
+                        dygraph_helper_horizontalLine(ctx, graph, 2.5, 'red');
+                    }
+                }; 
+
+                new Dygraph(document.getElementById(dygraph_element_id),
+                        csv, dygraph_opts);
+            }
+        });
+    }
+
 
     /*******************************************************/
     /* MFP INTEGRATION */
@@ -199,9 +317,14 @@ class DumbHouseApp {
 
     plot_mfp_diary(dygraph_element_id, days, daily_target_cals, diary) {
         var csv = "Date,Calories,Weight\n";
+        var weight_min = 999;
+        var weight_max = 0;
         for (var day of days) {
             var log = diary[day];
             csv += day + "," + log.calories + "," + (log.weight? log.weight : '') + "\n";
+
+            if (log.weight && log.weight < weight_min) weight_min = log.weight; 
+            if (log.weight && log.weight > weight_max) weight_max = log.weight; 
         }
 
         new Dygraph(
@@ -214,8 +337,9 @@ class DumbHouseApp {
                   'Weight': {axis: 'y2'},
               },
               axes: {
-                y : { valueRange: [0, 3000], },
-                y2: { valueRange: [70, 82], },
+                // Carefully picked ranges to make the graph look pretty
+                y : { valueRange: [daily_target_cals*0.4, daily_target_cals*1.5], },
+                y2: { valueRange: [weight_min * 0.98, weight_max * 1.12], },
               },
               strokeWidth: 3,
               pointSize : 5,
@@ -223,19 +347,9 @@ class DumbHouseApp {
               highlightCircleSize: 5,
               highlightSeriesOpts: {strokeWidth: 5, highlightCircleSize: 10},
               underlayCallback: function(ctx, area, graph) {
-                  function horizontalLine(y_val, color) {
-                      var xl = graph.toDomCoords(0,y_val);
-                      ctx.strokeStyle= color;
-                      ctx.beginPath();
-                      ctx.moveTo(xl[0],xl[1]);
-                      ctx.lineTo(9e22,xl[1]);
-                      ctx.closePath();
-                      ctx.stroke();      
-                  }
-
-                  horizontalLine(daily_target_cals*0.8, 'yellow');
-                  horizontalLine(daily_target_cals, 'green');
-                  horizontalLine(daily_target_cals*1.1, 'red');
+                  dygraph_helper_horizontalLine(ctx, graph, daily_target_cals*0.8, 'yellow');
+                  dygraph_helper_horizontalLine(ctx, graph, daily_target_cals, 'green');
+                  dygraph_helper_horizontalLine(ctx, graph, daily_target_cals*1.1, 'red');
               },
           }
         );
