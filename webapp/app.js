0bc5ccdbc22971b976d2beb98a3d579fe8e06de3
Nico <nicolasbrailo@gmail.com>
2019-04-27 20:13:40 +0200

    Add Nico landingpage

--- webapp/app.js
+++ webapp/app.js
@@ -60,23 +60,33 @@ class DumbHouseApp {
         if (this.baticueva_extras_open) $('#Baticueva_TV_extras_div').show();
     }
 
-    invoke_media_player_action(player_name, url) {
-        var self = this;
+    invoke_action(url, cb) {
         $.ajax({
             url: url,
             cache: false,
             type: 'get',
-            dataType: 'html',
+            dataType: 'json',
             success: function(action_res) {
+                if (cb) {
+                    cb(action_res);
+                } else {
+                    console.log(url, " => ", action_res);
+                }
+            }
+        });
+
+        return false;
+    }
+
+    invoke_media_player_action(player_name, url) {
+        var self = this;
+        return this.invoke_action(url, function(res) {
                 console.log(player_name, ".", url, " => ", action_res);
                 // Try to trigger a UI update in a second
                 // TODO: This doesn't actually work: updateUI only recreates the UI, doesn't fetch new state
                 // A call to updateStatus would be smarter
                 setTimeout(function(){ self.get_player(player_name).updateUI(); }, 2000);
-            }
         });
-
-        return false;
     }
 
     show_list_of_media_players(element_selector) {
@@ -123,6 +133,112 @@ class DumbHouseApp {
                 });
             }
         });
+    }
+
+
+    /*******************************************************/
+    /* MFP INTEGRATION */
+    /*******************************************************/
+    get_mfp_stats(daily_target_cals, callback) {
+        var self = this;
+        $.ajax({
+            url: "/mfp/stats",
+            cache: false,
+            type: 'get',
+            dataType: 'json',
+            success: function(diary) {
+                var stats = self.aggregate_mfp_diary(daily_target_cals, diary);
+
+                var recommend = ""
+                if (stats.period_target_cals_pct > 105) recommend = "<li><b>eat less</b></li>";
+                if (stats.period_target_cals_pct <  80) recommend = "<li><b>eat more</b></li>";
+
+                var mfp_summary = {
+                        title: "MFP "+stats.period_day_count+" day stats",
+                        text: "<ul>" +
+                            "<li>avg " + stats.daily_average_cals + " cal/day " +
+                            "("+ stats.period_target_cals_pct +"%)</li>" +
+                            "<li>trained "+stats.period_exercise_days+" days out of "+stats.period_day_count+
+                            " ("+stats.period_exercise_time+"min)</li>" +
+                            recommend +
+                            "</ul>",
+                };
+
+                var days = [];
+                for (var d in diary) days.push(d);
+                days = days.sort();
+
+                callback(days, diary, stats, mfp_summary);
+            },
+        });
+    }
+
+    aggregate_mfp_diary(daily_target_cals, diary) {
+        var stats = {
+                daily_target_cals: daily_target_cals,
+                period_day_count: 0,
+                period_exercise_days: 0,
+                period_exercise_time: 0,
+                period_total_cals: 0,
+        }
+
+        $.each(diary, function(day, log) {
+            stats.period_day_count += 1;
+            stats.period_total_cals += log.calories;
+            if (log.exercise_minutes > 0) {
+                stats.period_exercise_days += 1;
+                stats.period_exercise_time += log.exercise_minutes;
+            }
+        });
+
+        stats.daily_average_cals = stats.period_total_cals / stats.period_day_count;
+        stats.period_target_cals = stats.daily_target_cals * stats.period_day_count;
+        stats.period_target_cals_pct = Math.ceil(100 * stats.period_total_cals / stats.period_target_cals);
+        return stats;
+    }
+
+    plot_mfp_diary(dygraph_element_id, days, daily_target_cals, diary) {
+        var csv = "Date,Calories,Weight\n";
+        for (var day of days) {
+            var log = diary[day];
+            csv += day + "," + log.calories + "," + (log.weight? log.weight : '') + "\n";
+        }
+
+        new Dygraph(
+          document.getElementById(dygraph_element_id), csv,
+          {
+              ylabel: 'Calories',
+              y2label: 'Weight',
+              series: {
+                  'Calories': {axis: 'y'},
+                  'Weight': {axis: 'y2'},
+              },
+              axes: {
+                y : { valueRange: [0, 3000], },
+                y2: { valueRange: [70, 82], },
+              },
+              strokeWidth: 3,
+              pointSize : 5,
+              drawPoints: true,
+              highlightCircleSize: 5,
+              highlightSeriesOpts: {strokeWidth: 5, highlightCircleSize: 10},
+              underlayCallback: function(ctx, area, graph) {
+                  function horizontalLine(y_val, color) {
+                      var xl = graph.toDomCoords(0,y_val);
+                      ctx.strokeStyle= color;
+                      ctx.beginPath();
+                      ctx.moveTo(xl[0],xl[1]);
+                      ctx.lineTo(9e22,xl[1]);
+                      ctx.closePath();
+                      ctx.stroke();      
+                  }
+
+                  horizontalLine(daily_target_cals*0.8, 'yellow');
+                  horizontalLine(daily_target_cals, 'green');
+                  horizontalLine(daily_target_cals*1.1, 'red');
+              },
+          }
+        );
     }
 }
 
