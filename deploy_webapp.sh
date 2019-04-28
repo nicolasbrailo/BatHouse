0a9961f290a3587b69995cb5775a7ddf68773603
Nico <nicolasbrailo@gmail.com>
2019-04-28 19:25:43 +0200

    Refactor

--- deploy_webapp.sh
+++ deploy_webapp.sh
@@ -1,40 +1,35 @@
+# Install dependencies for this script:
 # sudo apt install npm
-# npm install handlebars -g
-
-# node node_modules/.bin/handlebars -k each -k if -k selectIfEq -k media_player_formatSeconds ./templates/*
+# npm install handlebars
 
 TARGET_JS=bathouse.minified.js
 TARGET_CSS=bathouse.minified.css
 
+# Clean (make sure loading old scripts fail)
 rm webapp/*$TARGET_JS
 rm webapp/*$TARGET_CSS
 
 echo "" > tmp.$TARGET_JS
 echo "" > tmp.$TARGET_CSS
 
-wget http://cdnjs.cloudflare.com/ajax/libs/dygraph/2.1.0/dygraph.min.js -qO- >> tmp.$TARGET_JS
-echo "" >> tmp.$TARGET_JS
-wget http://dygraphs.com/extras/smooth-plotter.js -qO- >> tmp.$TARGET_JS
-wget https://code.jquery.com/jquery-3.2.1.min.js -qO- >> tmp.$TARGET_JS
-wget https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.1/handlebars.min.js -qO- >> tmp.$TARGET_JS
+# Compile templates
+node node_modules/.bin/handlebars \
+        -k each -k if -k selectIfEq -k media_player_formatSeconds \
+        ./webapp/templates/* >> tmp.$TARGET_JS
 
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/js/bootstrap.min.js >> tmp.$TARGET_JS
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/things/app.js >> tmp.$TARGET_JS
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/things/templated_thing.js >> tmp.$TARGET_JS
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/things/media_player/model.js >> tmp.$TARGET_JS
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/things/mqtt_device_info/model.js >> tmp.$TARGET_JS
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/things/lamp/model.js >> tmp.$TARGET_JS
-minify ./webapp/app.js >> tmp.$TARGET_JS
+# Minify app dependencies
+minify ./webapp/js/graphs.js >> tmp.$TARGET_JS
+minify ./webapp/js/templated_thing.js >> tmp.$TARGET_JS
+minify ./webapp/js/scene_list.js >> tmp.$TARGET_JS
+minify ./webapp/js/mqtt_device_info.js >> tmp.$TARGET_JS
+minify ./webapp/js/media_player.js >> tmp.$TARGET_JS
+minify ./webapp/js/lamp.js >> tmp.$TARGET_JS
+minify ./webapp/js/baticueva_tv_extras.js >> tmp.$TARGET_JS
+minify ./webapp/js/mfp.js >> tmp.$TARGET_JS
+minify ./webapp/js/app.js >> tmp.$TARGET_JS
 
-
-wget http://cdnjs.cloudflare.com/ajax/libs/dygraph/2.1.0/dygraph.min.css -qO- >> tmp.$TARGET_CSS
-
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/css/bootstrap-theme.min.css >> tmp.$TARGET_CSS
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/css/bootstrap.min.css >> tmp.$TARGET_CSS
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/things/media_player/style.css >> tmp.$TARGET_CSS
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/things/mqtt_device_info/style.css >> tmp.$TARGET_CSS
-minify ./zigbee2mqtt2flask/zigbee2mqtt2flask/webapp/things/lamp/style.css >> tmp.$TARGET_CSS
-minify ./webapp/custom_styles.css >> tmp.$TARGET_CSS
+# Minify css files 
+minify ./webapp/css/* >> tmp.$TARGET_CSS
 
 mv tmp.$TARGET_JS  ./webapp/`md5sum tmp.$TARGET_JS  | cut -c 1-10`.$TARGET_JS
 mv tmp.$TARGET_CSS ./webapp/`md5sum tmp.$TARGET_CSS | cut -c 1-10`.$TARGET_CSS
