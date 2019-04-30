3bca976354ca416b9ed09eddb19f44332630557f
Nico <nicolasbrailo@gmail.com>
2019-04-30 17:18:31 +0200

    Adding auto-redeploy script

--- deploy_webapp.sh
+++ deploy_webapp.sh
@@ -4,37 +4,56 @@
 
 TARGET_JS=bathouse.minified.js
 TARGET_CSS=bathouse.minified.css
+DEPLOY_PATH=webapp
+
+PATH_OLD_JS=`ls $DEPLOY_PATH/*$TARGET_JS || echo 0000000NOOLDFILE`
+PATH_OLD_CSS=`ls $DEPLOY_PATH/*$TARGET_CSS || echo 0000000NOOLDFILE`
+HASH_OLD_JS=`echo $PATH_OLD_JS | cut -c8-17`
+HASH_OLD_CSS=`echo $PATH_OLD_CSS | cut -c8-17`
+echo "Old JS hash is $HASH_OLD_JS, old CSS hash is $HASH_OLD_CSS."
 
 # Clean (make sure loading old scripts fail)
-rm webapp/*$TARGET_JS
-rm webapp/*$TARGET_CSS
+rm $DEPLOY_PATH/*$TARGET_JS
+rm $DEPLOY_PATH/*$TARGET_CSS
 
 echo "" > tmp.$TARGET_JS
 echo "" > tmp.$TARGET_CSS
 
-# Compile templates
+echo "Compiling Handlebar templates"
 node node_modules/.bin/handlebars \
         -k each -k if -k selectIfEq -k media_player_formatSeconds \
-        ./webapp/templates/* >> tmp.$TARGET_JS
+        ./$DEPLOY_PATH/templates/* >> tmp.$TARGET_JS
 
-# Minify app dependencies
-minify ./webapp/js/graphs.js >> tmp.$TARGET_JS
-minify ./webapp/js/templated_thing.js >> tmp.$TARGET_JS
-minify ./webapp/js/scene_list.js >> tmp.$TARGET_JS
-minify ./webapp/js/mqtt_device_info.js >> tmp.$TARGET_JS
-minify ./webapp/js/media_player.js >> tmp.$TARGET_JS
-minify ./webapp/js/lamp.js >> tmp.$TARGET_JS
-minify ./webapp/js/baticueva_tv_extras.js >> tmp.$TARGET_JS
-minify ./webapp/js/mfp.js >> tmp.$TARGET_JS
-minify ./webapp/js/app.js >> tmp.$TARGET_JS
+echo "Minifying app .js files"
+minify ./$DEPLOY_PATH/js/graphs.js >> tmp.$TARGET_JS
+minify ./$DEPLOY_PATH/js/templated_thing.js >> tmp.$TARGET_JS
+minify ./$DEPLOY_PATH/js/scene_list.js >> tmp.$TARGET_JS
+minify ./$DEPLOY_PATH/js/mqtt_device_info.js >> tmp.$TARGET_JS
+minify ./$DEPLOY_PATH/js/media_player.js >> tmp.$TARGET_JS
+minify ./$DEPLOY_PATH/js/lamp.js >> tmp.$TARGET_JS
+minify ./$DEPLOY_PATH/js/baticueva_tv_extras.js >> tmp.$TARGET_JS
+minify ./$DEPLOY_PATH/js/mfp.js >> tmp.$TARGET_JS
+minify ./$DEPLOY_PATH/js/app.js >> tmp.$TARGET_JS
 
 # Minify css files 
-minify ./webapp/css/* >> tmp.$TARGET_CSS
+echo "Minifying app .css files"
+minify ./$DEPLOY_PATH/css/* >> tmp.$TARGET_CSS
 
-mv tmp.$TARGET_JS  ./webapp/`md5sum tmp.$TARGET_JS  | cut -c 1-10`.$TARGET_JS
-mv tmp.$TARGET_CSS ./webapp/`md5sum tmp.$TARGET_CSS | cut -c 1-10`.$TARGET_CSS
+HASH_NEW_JS=`md5sum tmp.$TARGET_JS | cut -c 1-10`
+HASH_NEW_CSS=`md5sum tmp.$TARGET_CSS | cut -c 1-10`
 
-echo "Created files:"
-ls webapp/*$TARGET_JS
-ls webapp/*$TARGET_CSS
+echo "New JS hash is $HASH_NEW_JS, new CSS hash is $HASH_NEW_CSS."
+for fn in `grep $HASH_OLD_JS $DEPLOY_PATH/*.html | awk -F':' '{print $1}'`; do
+    echo "Warning: $fn will break. Patching hash..."
+    sed -i -e "s/$HASH_OLD_JS/$HASH_NEW_JS/g" $fn
+    sed -i -e "s/$HASH_OLD_CSS/$HASH_NEW_CSS/g" $fn
+    echo "Fixed!"
+done
+
+mv tmp.$TARGET_JS  ./$DEPLOY_PATH/$HASH_NEW_JS.$TARGET_JS
+mv tmp.$TARGET_CSS ./$DEPLOY_PATH/$HASH_NEW_CSS.$TARGET_CSS
+
+echo "Deployed:"
+ls $DEPLOY_PATH/*$TARGET_JS
+ls $DEPLOY_PATH/*$TARGET_CSS
 
