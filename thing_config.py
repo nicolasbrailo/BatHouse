f4bfceaa18b53049dd90369519b8c6fc4ecfbb88
Nico Brailovsky <nicolasbrailo@gmail.com>
2021-06-01 09:54:05 +0100

    Add support for Ikea motion sensors

--- thing_config.py
+++ thing_config.py
@@ -1,20 +1,34 @@
 from zigbee2mqtt2flask.zigbee2mqtt2flask.things import Thing, Lamp, DimmableLamp, ColorDimmableLamp, ColorTempDimmableLamp, Button, MultiIkeaMotionSensor
 
+from astral.sun import sun as astral_sun
+import astral
+import datetime
 import threading
 import time
 
-def is_it_light_outside():
-    import datetime
-    import astral
-    from astral.sun import sun as astral_sun
+MY_LAT=51.5464371
+MY_LON=0.111148
+LATE_NIGHT_START_HOUR=22
 
-    t = astral_sun(astral.Observer(51.5464371, 0.111148), date=datetime.date.today())
+def is_it_light_outside():
+    t = astral_sun(astral.Observer(MY_LAT, MY_LON), date=datetime.date.today())
     tolerance = datetime.timedelta(minutes=45)
     sunrise = t['sunrise'] + tolerance
     sunset = t['sunset'] - tolerance
     ahora = datetime.datetime.now(t['sunset'].tzinfo)
     sun_out = ahora > sunrise and ahora < sunset
     return sun_out
+
+
+def is_it_late_night():
+    t = astral_sun(astral.Observer(MY_LAT, MY_LON), date=datetime.date.today())
+    sunset = t['dusk']
+    ahora = datetime.datetime.now(t['sunset'].tzinfo)
+    if ahora < sunset:
+        return False
+    if ahora.hour > LATE_NIGHT_START_HOUR:
+        return True
+    return False
 
 
 # Use the same logger as ZMF things
@@ -75,9 +89,9 @@ class IkeaButton2(Button):
         logger.warning("Unknown action: Ikea button - ", action)
 
 
-class Foo(MultiIkeaMotionSensor):
-    def __init__(self, world, mqtt_ids, light):
-        super().__init__(world, mqtt_ids)
+class MotionActivatedLight(MultiIkeaMotionSensor):
+    def __init__(self, world, sensor_mqtt_ids, light):
+        super().__init__(world, sensor_mqtt_ids)
         self.light = light
         self.light_on_because_activity = False
 
@@ -86,9 +100,9 @@ class Foo(MultiIkeaMotionSensor):
             return
 
         if not self.light.is_on:
+            brightness = 10 if is_it_late_night() else 50
             self.light_on_because_activity = True
-            self.light.set_brightness(30, broadcast_update=False)
-            self.light.broadcast_new_state(transition_time=3)
+            self.light.set_brightness(brightness)
 
     def all_vacant(self):
         if self.light_on_because_activity:
@@ -106,5 +120,5 @@ def register_all_things(world, scenes):
     world.register_thing(DimmableLamp('BatBedsideLamp', world.mqtt))
     world.register_thing(IkeaButton('BotonIkeaBelen', world, scenes))
     world.register_thing(IkeaButton2('BotonIkeaComedor', world, scenes))
-    world.register_thing(Foo(world, ['IkeaMotionSensorUpstairs','IkeaMotionSensorEntrepiso'], world.get_thing_by_name('EntrepisoLamp')))
+    world.register_thing(MotionActivatedLight(world, ['IkeaMotionSensorUpstairs','IkeaMotionSensorEntrepiso'], world.get_thing_by_name('EntrepisoLamp')))
 
