aff0535117e708055b71356ccf6182fca9a74fe1
Nico Brailovsky <nicolasbrailo@gmail.com>
2022-01-30 17:26:39 +0000

    Add instructions for fw update

---
+++ zigbee_fw_update/flash.sh
@@ -0,0 +1,1 @@
+python3 -m pipenv run python ./cc2538-bsl/cc2538-bsl.py  -p /dev/ttyUSB0 -Vvew ./CC2652R_coordinator_20220103.hex
---
+++ zigbee_fw_update/test.sh
@@ -0,0 +1,2 @@
+echo "This will test if the zigbee controller's uart works. Use it for sanity check before and after flashing"
+python3 -m pipenv run python znp-uart-test.py 
