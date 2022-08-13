9e365c90fbd3eab4e2e1d27ae097061c167da7ed
Nico Brailovsky <nicolasbrailo@gmail.com>
2022-08-13 12:09:43 +0100

    .

--- device_scripts/remove_device_force_interview.sh
+++ device_scripts/remove_device_force_interview.sh
@@ -1,4 +1,4 @@
 echo "This will make a device unknown to zigbee2mqtt, forcing an interview to happen"
 echo "Danger: Deletes info from your config. Uncomment code below to use or copypaste:"
-echo 'mosquitto_pub -t zigbee2mqtt/bridge/request/device/remove -m {"id": "ID"}'
-# mosquitto_pub -t zigbee2mqtt/bridge/request/device/remove -m {"id": "$1"}
+echo mosquitto_pub -t zigbee2mqtt/bridge/request/device/remove -m {"id": "$1","force":true}
+# mosquitto_pub -t zigbee2mqtt/bridge/request/device/remove -m {"id": "$1","force":true}
